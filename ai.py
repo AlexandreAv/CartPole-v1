60a373dcdb5fd204e3eb6bd787ea8a34d382752a
Alexandre Rangom <alexandre.rangom@gmail.com>
2019-10-27 19:56:02 +0100

    Tentative de correction 1 + ajout de done dans la fonctino Q

--- ai.py
+++ ai.py
@@ -8,7 +8,7 @@ from random import random, randrange, sample
 tf.keras.backend.set_floatx('float64')
 EPISLON = 0.01
 GAMMA = 0.95
-LR = 0.0005
+LR = 0.005
 INPUT_SIZE = 4
 OUTPUT_SIZE = 2
 BATCH_SIZE = 32
@@ -50,10 +50,10 @@ class Memory:
         self.batch_size = batch_size
         self.memory_max = memory_max
 
-    def add(self, state, next_state, action, reward):
+    def add(self, state, next_state, action, reward, done):
         if self.memory_max == len(self.storage):  # On supprimer le premier élément en mémoire si on atteint la capacité max
             del self.storage[0]
-        self.storage.append([state, next_state, action, reward])  # On enrengistre les actions dans la mémoire
+        self.storage.append([state, next_state, action, reward, done])  # On enrengistre les actions dans la mémoire
 
     def get_sample(self):
         batch = list(zip(*sample(self.storage, self.batch_size)))  # On regroupe les éléments en liste de state, liste de next_state, liste d'action et liste de reward
@@ -66,6 +66,7 @@ class Memory:
         batch[1] = tf.convert_to_tensor(batch[1], dtype=tf.float64)  # voir au dessus
         batch[2] = tf.expand_dims(tf.convert_to_tensor(batch[2], dtype=tf.float64), 1)  # on transforme en tensor et ajoute une dimension la compatibilté des shapes pour les calculs
         batch[3] = tf.expand_dims(tf.convert_to_tensor(batch[3], dtype=tf.float64), 1)  # voir au dessus
+        batch[4] = tf.expand_dims(tf.convert_to_tensor(batch[4], dtype=tf.float64), 1)
 
         return batch
 
@@ -89,14 +90,14 @@ class DQN:
         self.metrics_loss = tf.metrics.MeanSquaredError()
 
     @tf.function
-    def train(self, batch_states, batch_next_states, batch_actions, batch_reward):
+    def train(self, batch_states, batch_next_states, batch_actions, batch_reward, batch_done):
 
         next_action_max = tf.reduce_max(self.model(batch_next_states))  # Q(s', a', 0)
         q_targets = tf.add(batch_reward, tf.scalar_mul(GAMMA, next_action_max))  # Calcul de la target, r + GAMMA * Q(s', a', 0)*
         # pdb.set_trace()
 
         with tf.GradientTape() as tape:  # On prépare le calcul du gradient
-            predictions = tf.reduce_max(self.model(batch_states))  # Q(s, a, 0)
+            predictions = tf.reduce_max(self.model(batch_states)) * (1 - batch_done) # Q(s, a, 0)
             loss = tf.keras.losses.MSE(q_targets, predictions)  # Calcul de l'erreur
 
         grads = tape.gradient(loss, self.model.trainable_variables)  # Calcul du gradient
@@ -104,16 +105,16 @@ class DQN:
 
         self.metrics_loss(q_targets, predictions)
 
-    def select_action(self, signals, reward):
+    def select_action(self, signals, reward, done):
         self.state = self.next_state  # On récupère l'état actuelle
         self.next_state = np.array([signals])  # On récupère le nouvelle état
         action = self.model.pick_action(self.next_state, EPISLON)  # On choisit une action
 
-        self.memory.add(self.state, self.next_state, action, reward)  # On ajoute une transition à la mémoire
+        self.memory.add(self.state, self.next_state, action, reward, done)  # On ajoute une transition à la mémoire
 
         if self.memory.is_enough_data():  # On entraine notre modèle si il y a assez de données
-            batch_states, batch_next_states, batch_actions, batch_reward = self.memory.get_sample()  # On récupère notre lot de données
-            self.train(batch_states, batch_next_states, batch_actions, batch_reward)
+            batch_states, batch_next_states, batch_actions, batch_reward, batch_done = self.memory.get_sample()  # On récupère notre lot de données
+            self.train(batch_states, batch_next_states, batch_actions, batch_reward, batch_done)
             print('------------------------------------------')
             print("la moyenne des erreurs est de %s" % self.metrics_loss.result())  # On affiche la moyenne des erreurs non cumulées
             print("le reward est de %s" % self.memory.last_reward())  # On affiche le dernier rewarc
--- main.py
+++ main.py
@@ -8,9 +8,10 @@ env = gym.make('CartPole-v1')
 for i_episode in range(20000):
     observation = env.reset()
     reward = 0
+    done = False
     for t in range(500):
         env.render()
-        action = AI.select_action(observation, reward)
+        action = AI.select_action(observation, reward, done)
         observation, reward, done, info = env.step(action)
         if done:
             print("Episode finished after {} timesteps".format(t+1))
